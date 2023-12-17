00a6c43b43fac6447bc0a7d3375eff24bfae4251
Dhruv Chandak <dhruvchandak5@gmail.com>
2023-12-17 19:39:40 +0530

    Added content to Navbar

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import "./Navbar.css";
 import HamsBurger from "./HamsBurger";
 import { Links } from "../../data/data";
+import Logo from "./logo.jpg";
 
 const Navbar = () => {
   const [screenSize, setScreenSize] = useState(false);
@@ -29,44 +30,45 @@ const Navbar = () => {
 
   return (
     <div className={`w-screen bg-black fixed`}>
-      <div
-        className={`flex text-xl px-6 py-4 justify-between items-center`}
-      >
+      <div className={`flex text-xl px-6 py-4 justify-between items-center`}>
         {/* Logo */}
         <div className="-z-50">
           <Link to="/">
-            <h4 className="text-red-800 font-bold text-xl cursor-pointer">LNMIIT Library</h4>
+            {/* <h4 className="text-red-800 font-bold text-xl cursor-pointer"> */}
+              <img src={Logo} alt="LNMIIT" className="w-1/2"></img>
+            {/* </h4> */}
           </Link>
         </div>
         {/* Logo */}
 
         {/* Other navigation buttons */}
         <div className={``}>
-        {screenSize ? (
+          {screenSize ? (
             <HamsBurger
               hamsburgerhandler={hamsburgerhandler}
               status={showmenu}
             />
-          ):(
-            <ul className={`flex gap-x-8 text-lg z-20 font-semibold text-white ${screenSize ? "hidden" : "block"}`}>
-              {
-                Links?.map((e)=>(
-                  <li className="cursor-pointer relative inline-block group">
-              <Link to={e.route}>
-                {e.title}
-                <span className="absolute w-full transform scale-x-0 h-[2px] bottom-0 left-0 bg-white transform-origin-bottom-right transition-transform duration-250 ease-out group-hover:transform-origin-bottom-left group-hover:scale-x-100"></span>
-              </Link>
-            </li>
-                ))
-              }
-            {/* <li className="flex items-center text-lg font-normal gap-x-4 button-grp text-richblack-100">
+          ) : (
+            <ul
+              className={`flex gap-x-8 text-lg z-20 font-semibold text-white ${
+                screenSize ? "hidden" : "block"
+              }`}
+            >
+              {Links?.map((e) => (
+                <li className="cursor-pointer relative inline-block group">
+                  <Link to={e.route}>
+                    {e.title}
+                    <span className="absolute w-full transform scale-x-0 h-[2px] bottom-0 left-0 bg-white transform-origin-bottom-right transition-transform duration-250 ease-out group-hover:transform-origin-bottom-left group-hover:scale-x-100"></span>
+                  </Link>
+                </li>
+              ))}
+              {/* <li className="flex items-center text-lg font-normal gap-x-4 button-grp text-richblack-100">
             <Link to="/login" onClick={() => setShowmenu(false)}>
                 <button className="text-white">Register</button>
               </Link>
             </li>  */}
-          </ul>
+            </ul>
           )}
-          
         </div>
         {/* Other navigation buttons */}
       </div>
@@ -82,15 +84,13 @@ const Navbar = () => {
             {/* <li>
               <Link to="/" onClick={()=>setShowmenu(false)}>Home</Link>
             </li> */}
-             {
-                Links?.map((e)=>(
-                  <li>
-                  <Link to={e.route} onClick={() => setShowmenu(false)}>
-                    {e.title}
-                  </Link>
-                </li>
-                ))
-              }
+            {Links?.map((e) => (
+              <li>
+                <Link to={e.route} onClick={() => setShowmenu(false)}>
+                  {e.title}
+                </Link>
+              </li>
+            ))}
           </ul>
           {/* <div className="flex flex-col gap-y-4 button-grp">
           <Link to="/login" onClick={() => setShowmenu(false)}>
--- client/src/data/data.js
+++ client/src/data/data.js
@@ -1,26 +1,27 @@
-export const Links=[
-    {
-        title:"section 1",
-        route:"/home"
-    },
-    {
-        title:"section 2",
-        route:"/home"
-    },
-    {
-        title:"section 3",
-        route:"/home"
-    },
-    {
-        title:"section 4",
-        route:"/home"
-    },
-    {
-        title:"section 5",
-        route:"/home"
-    },
-    {
-        title:"section 6",
-        route:"/home"
-    },
-]
+export const Links = [
+  {
+    title: "Home",
+    route: "/home",
+  },
+  {
+    title: "Services",
+    route: "/home",
+  },
+  {
+    title: "Collection",
+    route: "/home",
+  },
+  {
+    title: "Research",
+    route: "/home",
+  },
+  {
+    title: "About Us",
+    route: "/home",
+  },
+
+  {
+    title: "Contact Us",
+    route: "/home",
+  },
+];
